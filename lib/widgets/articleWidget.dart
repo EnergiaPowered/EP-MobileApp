0687c756c0b1ec9e78bc081eeaab90a0a75548b3
alrashidi7 <mohaalrashidi@gmail.ocm>
2020-10-26 15:00:24 +0200

    EnergiaApp

---
+++ lib/widgets/articleWidget.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ArticleWidget extends StatefulWidget {
+  @override
+  _ArticleWidgetState createState() => _ArticleWidgetState();
+}
+
+class _ArticleWidgetState extends State<ArticleWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
